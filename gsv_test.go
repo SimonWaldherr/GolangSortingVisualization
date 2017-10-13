da4f5493ea4765b4003ea7a8ea4a9346bf5d8b03
James Pudney <jcpudney@gmail.com>
2017-10-13 21:27:29 +0100

    Implement Nop FrameGen Writer.

--- gsv_test.go
+++ gsv_test.go
@@ -103,9 +103,13 @@ func Benchmark_sleep_sort(b *testing.B)     { benchmarkSort("sleep", b) }
 func Benchmark_stooge_sort(b *testing.B)    { benchmarkSort("stooge", b) }
 func Benchmark_quick_sort(b *testing.B)     { benchmarkSort("quick", b) }
 
+// WriteNop is a writer for FrameGen that does nothing.
+// Ensures we only benchmark alogrithms.
+func WriteNop(_ []int) {}
+
 func benchmarkSort(sort string, b *testing.B) {
 	arr := randomArray(Count, Max)
-	frameGen := FrameGen(WriteStdout)
+	frameGen := FrameGen(WriteNop)
 	if sort, found := sorterMap[sort]; found {
 		for n := 0; n < b.N; n++ {
 			sort(arr, frameGen)
