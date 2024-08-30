a9f8af52e589faa1a3aacb4adc39a4a3dc652f62
Simon Waldherr <git@simon.waldherr.eu>
2024-08-30 20:46:17 +0200

    add radix

--- gsv.go
+++ gsv.go
@@ -663,3 +663,53 @@ func siftDown(arr []int, start, end int, frameGen FrameGen) {
 		}
 	}
 }
+
+// RadixSort is an implementation of https://en.wikipedia.org/wiki/Radix_sort
+func RadixSort(arr []int, frameGen FrameGen) {
+	if frameGen != nil {
+		frameGen(arr)
+	}
+	radixSort(arr, frameGen)
+}
+
+func radixSort(arr []int, frameGen FrameGen) {
+	max := max(arr)
+	for exp := 1; max/exp > 0; exp *= 10 {
+		countingSort(arr, exp, frameGen)
+	}
+}
+
+func countingSort(arr []int, exp int, frameGen FrameGen) {
+	output := make([]int, len(arr))
+	count := make([]int, 10)
+
+	for i := 0; i < len(arr); i++ {
+		count[(arr[i]/exp)%10]++
+	}
+
+	for i := 1; i < 10; i++ {
+		count[i] += count[i-1]
+	}
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		output[count[(arr[i]/exp)%10]-1] = arr[i]
+		count[(arr[i]/exp)%10]--
+	}
+
+	for i := 0; i < len(arr); i++ {
+		arr[i] = output[i]
+		if frameGen != nil {
+			frameGen(arr)
+		}
+	}
+}
+
+func max(arr []int) int {
+	max := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	return max
+}
